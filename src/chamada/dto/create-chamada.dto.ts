9e58353e2a327206236f4b76b4db6129b89a72c6
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-11-09 16:12:57 -0300

    changing dtos properties

--- src/chamada/dto/create-chamada.dto.ts
+++ src/chamada/dto/create-chamada.dto.ts
@@ -1,4 +1,39 @@
-import { Origem, Destino, Opcionais, Passageiro } from '@dtos/prop.dtos';
+class Origem {
+  readonly lat: number;
+  readonly lon: number;
+  readonly rua: string;
+  readonly numero: string;
+  readonly complemento?: string;
+  readonly observacoes: string;
+  readonly bairro: string;
+  readonly cidade: string;
+  readonly estado: string;
+}
+
+class Destino {
+  lat: number;
+  lon: number;
+  rua: string;
+  numero: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+}
+
+class Opcionais {
+  ar: boolean;
+  portamalas: boolean;
+  animal: boolean;
+  cadeirante: boolean;
+  premier: boolean;
+  categoria: number;
+}
+
+class Passageiro {
+  nome: string;
+  telefone: string;
+}
+
 export class CreateChamadaDto {
   origem: Origem;
   destino: Destino;
