a0a32b7ba1079e0179c3db84f2fe5272a4abff44
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-11-15 21:34:58 -0300

    setup typeorm with sqlite

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
@@ -10,6 +10,11 @@ export class UserController {
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    // return this.userService.create(createUserDto);
+    return this.userService.create(createUserDto);
+  }
+
+  @Get('user:email')
+  findByEmail(email: string) {
+    return this.userService.findByEmail(email);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  // async create(createUserDto: CreateUserDto): Promise<User> {
-  //   return `This action returns a new user primise`;
-  // }
+  constructor(
+    @Inject('USER_REPOSITORY')
+    private readonly userRepository: Repository<User>,
+  ) {}
 
-  findByEmail(email: string) {
-    return `This action return user by email`;
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.userRepository.create(createUserDto);
+    return this.userRepository.save(user);
+  }
+
+  async findByEmail(email: string) {
+    const user = await this.userRepository.findOne({ where: { email } });
+    if (!user) throw new NotFoundException('usuário nao encontrado');
+    return user;
   }
 }
