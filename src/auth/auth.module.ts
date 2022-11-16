e6c8ce76b8b2e48d4290b089dd02e9e1a428451b
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-11-16 01:30:19 -0300

    implements auth with: JWT, Guards, Middlewares, strategies, models & errors handling

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,29 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
+import { LoginValidationMiddleware } from './middlewares/login-validator.middleware';
+import { ConfigService } from '@nestjs/config';
 
+const config = new ConfigService();
 @Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: config.get('JWT_SECRET'),
+      signOptions: { expiresIn: '30d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoginValidationMiddleware).forRoutes('login');
+  }
+}
