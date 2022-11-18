90ea2e2af767da91b4d8ee35bb3342df863ca089
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-11-18 10:52:27 -0300

    change jwt strategy to get seceretOrKey from the environments

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,3 +1,4 @@
+import appConfig from '@configs/app.config';
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
@@ -10,7 +11,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: appConfig().appSecret,
     });
   }
 
