a85577fa0d5bfeaa3c40eae8704e0d5982bd44b8
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-11-30 23:08:00 -0300

    feature: refactor to typeorm environmets not's be hardcoding

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,8 +5,12 @@ export const databaseProviders = [
     provide: 'DATA_SOURCE',
     useFactory: async () => {
       const dataSource = new DataSource({
-        type: 'sqlite',
-        database: 'user-adm.sqlite',
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'vdt_root',
+        password: 'vdtch@m@d@taxista',
+        database: 'vdt_chamada_taxista',
         entities: [__dirname + '/../**/*.entity.{js,ts}'],
         synchronize: true,
       });
