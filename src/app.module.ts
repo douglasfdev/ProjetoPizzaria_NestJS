7cbea1d24f50c668f70ea45d488ecf9d4cd32851
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-11-30 23:08:00 -0300

    feature: refactor to typeorm environmets not's be hardcoding

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,35 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ChamadaModule } from './chamada/chamada.module';
 import { getEnvPath } from './common/helpers/env.helper';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('VDT_DB_HOST'),
+        port: config.get('VDT_DB_PORT'),
+        database: config.get('VDT_DB_DATABASE'),
+        username: config.get('VDT_DB_USER'),
+        password: config.get('VDT_DB_PASSWORD'),
+        migrations: ['dist/migrations/*.{ts,js}'],
+        logger: 'file',
+        synchronize: true, // never use TRUE in production!
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
+    }),
     ChamadaModule,
-    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    ConfigModule.forRoot({ envFilePath, isGlobal: true, cache: true }),
     UserModule,
     AuthModule,
   ],
