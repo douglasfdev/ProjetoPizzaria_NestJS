078cdeca8652145b36b29ed97883c61e874866fb
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-03-09 20:58:02 -0300

    feat(entity): add User entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule],
   controllers: [],
-  providers: [PrismaService],
+  providers: [],
 })
 export class AppModule {}
