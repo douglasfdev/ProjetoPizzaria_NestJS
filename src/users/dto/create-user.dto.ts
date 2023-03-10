4f0daeadabe061cea07688242bf71b9771c74611
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-03-09 21:46:43 -0300

    feat(repository): add repository

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,36 @@
-export class CreateUserDto {}
+import {
+  IsDate,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsStrongPassword({
+    minLength: 6,
+    minUppercase: 1,
+    minSymbols: 1,
+    minNumbers: 1,
+  })
+  password: string;
+
+  @IsDate()
+  created_at: Date;
+
+  @IsDate()
+  updated_at: Date;
+}
