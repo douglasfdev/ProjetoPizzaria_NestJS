ae3448dde4c09aecd0e167a8350b92534cbf1dcc
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-05-06 14:07:41 -0300

    chore(log): added utils nodeEnv to log in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { setupSwagger } from './docs/swagger.config';
-import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { config } from 'dotenv';
 import { getEnvPath } from '@helpers/env.helper';
+import log from '@utils/nodeEnv.util';
 
 const envFilePath = getEnvPath(`${__dirname}/../common/envs`);
 config({ path: envFilePath });
 
 (async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const logger = new Logger();
 
   app.enableVersioning({
     type: VersioningType.URI,
@@ -32,8 +32,6 @@ config({ path: envFilePath });
   setupSwagger(app);
 
   await app.listen(process.env.PORT || 5555, () => {
-    logger.log(`ENV: ${process.env.NODE_ENV}`);
-    logger.log(`PORT: ${process.env.PORT}`);
-    logger.log(`HOST: http://localhost:${process.env.PORT}/`);
+    log();
   });
 })();
