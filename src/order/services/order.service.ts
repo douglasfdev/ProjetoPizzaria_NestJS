32b69b1f3a2e643d7898091e757243246553f629
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-05-06 19:52:04 -0300

    feat(order): added a create, update and delete to order services

--- src/order/services/order.service.ts
+++ src/order/services/order.service.ts
@@ -3,22 +3,33 @@ import { CreateOrderDto } from '../dto/create-order.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Order } from '../entities/order.entity';
+import { OrderEnumType } from 'src/enum/OrderEnum';
 
 @Injectable()
 export class OrderService {
   constructor(
     @InjectRepository(Order)
-    private readonly repository: Repository<Order>,
+    private readonly orderRepo: Repository<Order>,
   ) {}
 
   async create(createOrderDto: CreateOrderDto) {
     const order = { ...createOrderDto };
 
-    const createdOrder = this.repository.create(order);
+    const createdOrder = this.orderRepo.create(order);
 
-    await this.repository.save(createdOrder);
+    await this.orderRepo.save(createdOrder);
     return {
       ...createOrderDto,
     };
+  }
+
+  async updateOrder(id: string, createOrderDto: CreateOrderDto) {
+    return this.orderRepo.update(id, createOrderDto);
+  }
+
+  async removeOrder(id: string) {
+    return this.orderRepo.update(id, {
+      status: OrderEnumType.CANCELED,
+    });
   }
 }
