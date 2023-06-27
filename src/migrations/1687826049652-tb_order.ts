57301e7744eca86fe8d04b7186b537289a4605d4
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-06-26 21:44:58 -0300

    feat(products): add find all products with they categories

---
+++ src/migrations/1687826049652-tb_order.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class TbOrder1687826049652 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
